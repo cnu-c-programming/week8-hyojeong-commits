7c72bac7b6feed37c20dce945f6eae91f738b03d
hyojeong-commits <kweunsungo@gmail.com>
2026-05-01 00:00:54 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -14,12 +14,12 @@ char *my_strcat(char *dst, const char *src) {
 }
 
 int main() {
-    char str1[20] = "Hello ";
+    char str1[20] = "Hello";
     char str2[] = "World";
-
+    
     printf("%s\n", str1);
     my_strcat(str1, str2);
     printf("%s\n", str1);
-
+    
     return 0;
 }
