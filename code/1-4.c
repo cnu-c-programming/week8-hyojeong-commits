a63f47a1ceef7a9f791ef8475deb9b3016563aad
hyojeong-commits <kweunsungo@gmail.com>
2026-04-30 23:29:21 +0900

    Update 1-4.c

--- code/1-4.c
+++ code/1-4.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
+int my_strcmp(const char *a, const char *b) {
+    while (*a != '\0' && (*a == *b)) {
+        a++; b++;
+    }
+    return *(unsigned char *)a - *(unsigned char *)b;
+}
+
 int main() {
-    char p[] = "hello";
-    p[0] = 'H';
-    printf("%s\n", p); 
+    
+    printf("%d\n", my_strcmp("abc", "abcd")); 
+    printf("%d\n", my_strcmp("abc", "abc")); 
+    printf("%d\n", my_strcmp("b", "a")); 
 
     return 0;
 }
